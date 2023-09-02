5e13fa322c45db821cb85f20e5baa527e92ae0ed
Steven <127547289+PaginasWeb100@users.noreply.github.com>
2023-09-02 17:35:57 -0500

    Add files via upload

---
+++ Js/AnimacionAmor.js
@@ -0,0 +1,11 @@
+function Redireccion(){
+    location.href="../Mi_Amor/Vista/CartitaDeAmor.html"
+}
+
+function cambiarMensaje(){
+    document.getElementById("CargandCarta").innerText="Abriendo cartitaaaa 😍...";
+}
+
+setTimeout(cambiarMensaje, 3000);
+
+setTimeout(Redireccion, 3600);
